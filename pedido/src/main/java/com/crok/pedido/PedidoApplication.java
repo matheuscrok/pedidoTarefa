b36da17224fc78292b4fd2dbe8732981ec6b9bbf
crok <zenorhan@hotmail.com>
2021-02-26 16:59:17 -0300

    ..

--- pedido/src/main/java/com/crok/pedido/PedidoApplication.java
+++ pedido/src/main/java/com/crok/pedido/PedidoApplication.java
@@ -3,6 +3,11 @@ package com.crok.pedido;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author CROK
+ *
+ */
 @SpringBootApplication
 public class PedidoApplication {
 
